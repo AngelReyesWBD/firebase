5e65c3ee2aa8ac1cd32afad090920526ec31ac23
wbd <reyesgalvezag@gmail.com>
2025-05-11 17:12:19 -0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "TU_API_KEY_AQUI",
+  authDomain: "estadoservicios.firebaseapp.com",
+  databaseURL: "https://estadoservicios-default-rtdb.firebaseio.com",
+  projectId: "estadoservicios",
+  storageBucket: "estadoservicios.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+// Inicializa Firebase
+firebase.initializeApp(firebaseConfig);
